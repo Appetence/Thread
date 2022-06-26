492f76d274a933c066d31fea8b8ff458f73b7942
liuhao <15834260040@163.com>
2022-06-26 10:11:06 +0800

    feat : test

--- src/thread/forkJoin/ForkJoinThreadExcutePoolLearn.java
+++ src/thread/forkJoin/ForkJoinThreadExcutePoolLearn.java
@@ -1,5 +1,9 @@
 package thread.forkJoin;
 
+import java.io.IOException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @program: ThreadLearn
  * @description:
@@ -7,4 +11,15 @@ package thread.forkJoin;
  * @date: 2022-05-31 16:38
  */
 public class ForkJoinThreadExcutePoolLearn {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        forkJoinPool.submit(() -> {
+            System.out.println(">>>>");
+        });
+        forkJoinPool.shutdown();
+        forkJoinPool.awaitTermination(1, TimeUnit.SECONDS);
+
+
+    }
 }
