f9ed68edc9c739322bb9d0973632b56cac738576
liuhao <15834260040@163.com>
2022-08-29 08:58:42 +0800

    feat: learn01

---
+++ src/leetcode/binary/Learn01.java
@@ -0,0 +1,34 @@
+package leetcode.binary;
+
+/**
+ * @program: ThreadLearn
+ * @description:
+ * @author: liuhao
+ * @date: 2022-08-29 08:13
+ */
+public class Learn01 {
+
+    /**
+     * 1 有序数组，查找某个数是否存在
+     * 2 有序数组 >= 某个数最左侧的位置
+     * 3 有序数组 <= 某个数最右侧的位置
+     * 4 相邻不相等，局部最小值问题
+     * @param args
+     */
+    public static void main(String[] args) {
+        findOwne(){
+            
+        }
+    }
+
+    private static void findOwne() {
+        git remote set-url origin https://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@github.com/Appetence/Thread.git
+         https://<your_token>@github.com/<USERNAME>/<REPO>.git
+         Updates were rejected because the remote contains work that you do
+         not have locally. This is usually caused by another repository pushing
+         to the same ref. You may want to first integrate the remote changes
+         (e.g., 'git pull ...') before pushing again.
+         See the 'Note about fast-forwards' in 'git push --help' for details.
+
+    }
+}
